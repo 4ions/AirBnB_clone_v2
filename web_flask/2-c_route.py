b10803adb3be3fd0aae45094b0c22733fce7869f
4ions <leovalenciasantana@gmail.com>
2021-05-04 22:27:55 -0500

    Fixed pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Minimal flask app"""
 
@@ -11,15 +10,18 @@ def web_flask():
     """Route index"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Route hbnb"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ Route c"""
     return "c %s" % text.replace("_", " ")
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000)
