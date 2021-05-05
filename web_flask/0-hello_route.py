11e6e5f26014c822da03f9a14c6e577e9eb57188
4ions <leovalenciasantana@gmail.com>
2021-05-04 21:48:29 -0500

    Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""Minimal flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def web_flask():
+    """Route index"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/python3
+"""Minimal flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def web_flask():
+    """Route index"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route hbnb"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Route c"""
+    return "c %s" % text.replace("_", " ")
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
