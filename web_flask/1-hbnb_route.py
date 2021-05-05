f230e72814348ad7f1aecdf1502a75cb68fb4cf6
4ions <leovalenciasantana@gmail.com>
2021-05-04 21:57:38 -0500

    Fixed pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
-
 #!/usr/bin/python3
 """Minimal flask app"""
+
 
 from flask import Flask
 app = Flask(__name__)
@@ -11,10 +11,12 @@ def web_flask():
     """Route index"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Route hbnb"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 5000)
